f4ef5c6a5dcc2d8f9b352deda544cc1636e1e898
mzhelyez <maksym.zhelyeznyakov@planopsim.com>
2025-09-13 15:02:54 +0200

    updated compile tests

--- test/test_reverse_mode_autodiff_special_functions_compile/gamma/test_reverse_mode_autodiff_tgamma1pm1_compile_test_ET_ON_mp_types.cpp
+++ test/test_reverse_mode_autodiff_special_functions_compile/gamma/test_reverse_mode_autodiff_tgamma1pm1_compile_test_ET_ON_mp_types.cpp
@@ -1,14 +1,15 @@
 //           Copyright Maksym Zhelyeznyakov 2025-2026
-//Distributed under the Boost Software License, Version 1.0.
-//(See accompanying file LICENSE_1_0.txt or copy at
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
 //           https://www.boost.org/LICENSE_1_0.txt)
 //
 // This file was generated automatically with math/tests/autogen_rvar_specfun_tests.sh
 // DO NOT EDIT MANUALLY
 
 #define BOOST_MATH_REVERSE_MODE_ET_ON
-#include "../../test_autodiff_reverse.hpp"
+#include <test_autodiff_reverse.hpp>
 #include <boost/math/special_functions.hpp>
+#include <boost/math/special_functions/logaddexp.hpp>
 #include <boost/math/tools/workaround.hpp>
 #include <cmath>
 
@@ -16,7 +17,7 @@ BOOST_AUTO_TEST_SUITE(test_tgamma1pm1_compiles)
 
 using namespace rdiff;
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(test_tgamma1pm1, T, bmp::cpp_bin_float_50)
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_tgamma1pm1, T, multiprecision_float_types)
 {
     RandomSample<T> rng{-3, 5};
     T               x = rng.next();
