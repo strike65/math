69d1fa7f82dc3123d55b4e0fcbba4fd3276f2f8a
mzhelyez <maksym.zhelyeznyakov@planopsim.com>
2025-09-13 15:22:37 +0200

    added license to python scripts

--- test/test_reverse_mode_autodiff_special_functions_compile/autogen_rvar_specfun_tests.py
+++ test/test_reverse_mode_autodiff_special_functions_compile/autogen_rvar_specfun_tests.py
@@ -5,7 +5,11 @@ Created on Thu Sep 11 16:10:41 2025
 
 @author: mzhelyez
 """
-
+#           Copyright Maksym Zhelyeznyakov 2025-2026
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+#           https://www.boost.org/LICENSE_1_0.txt)
+#
 import sys, os, subprocess
 import logging
 import re
--- test/test_reverse_mode_autodiff_special_functions_compile/update_docs_and_jamfile.py
+++ test/test_reverse_mode_autodiff_special_functions_compile/update_docs_and_jamfile.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 13 14:27:51 2025
-
-@author: mzhelyez
-"""
-
+#        Copyright Maksym Zhelyeznyakov 2025-2026
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+#           https://www.boost.org/LICENSE_1_0.txt)
+#
 import sys, os, subprocess
 import logging
 import re
